863ef0d677fa750e1ae72dd482b55eec1f0d9c66
parvezaman <emailtoamanullah@gmail.com>
2023-01-17 18:33:55 +0600

    fitst php code

---
+++ index.php
@@ -0,0 +1,22 @@
+<?php
+echo "hello world!";
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title><?php echo "i am a nube"; ?></title>
+</head>
+
+<body>
+    <h1>Hi lets learn PHP</h1>
+</body>
+
+</html>
+
+<?php 
+    echo (1+2);
