cc2912d0aa8614e3a67d12fdfac05a61762da56f
parvezaman <emailtoamanullah@gmail.com>
2023-01-19 11:40:55 +0600

    echo vs print

---
+++ echo.php
@@ -0,0 +1,4 @@
+<?php
+echo "learn PHP";
+echo "<br>";
+echo "<h2>This is a h2 heading</h2>";
---
+++ print.php
@@ -0,0 +1,12 @@
+<?php
+print("This is a print funciton");
+print("<h1>This is a print funciton</h1>");
+$text = "<h3> to print in echo </h3>";
+$var_print = print("hello from print");
+echo "<br>";
+print $text;
+echo "<br>";
+// $var_echo = echo .text;
+echo $text;
+
+// you can not assign 'echo' into other variable but you can call other variables into 'echo'; you can do both with "print"
